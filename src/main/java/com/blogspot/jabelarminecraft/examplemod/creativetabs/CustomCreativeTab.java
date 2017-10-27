6184de5c140d4bcaa3827b0043017ad12a6be196
jabelar <julian_abelar@hotmail.com>
2017-10-26 20:43:32 -0700

    A little clean up. Also trying out the version checking json system.

--- src/main/java/com/blogspot/jabelarminecraft/examplemod/creativetabs/CustomCreativeTab.java
+++ src/main/java/com/blogspot/jabelarminecraft/examplemod/creativetabs/CustomCreativeTab.java
@@ -24,15 +24,22 @@ import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-// TODO: Auto-generated Javadoc
+/**
+ * This class is used for an extra tab in the creative inventory. Many
+ * mods like to group their special items and blocks in a dedicated tab
+ * although it is also perfectly acceptable to put them in the vanilla tabs
+ * where it makes sense.
+ * */
 public class CustomCreativeTab extends CreativeTabs
 {
-
     /**
      * Instantiates a new custom creative tab.
      */
     public CustomCreativeTab()
     {
+        // pass a string for the tab label, if you only have one it is common
+        // to pass the modid and then in your lang file you can put name of your mod.
+        // The unlocalized name of a tab automatically has "itemGroup." prepended.
         super(MainMod.MODID);
     }
 
